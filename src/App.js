b030fba4b1d7e5a3b9e172801e9a89f17ff6e515
Brittany Halterman <halterma@gmail.com>
2025-03-09 12:03:22 -0400

    Update state

--- src/App.js
+++ src/App.js
@@ -4,7 +4,17 @@ import './App.css';
 function App() {
   const [score, setScore] = useState(0); // [0, ƒ]
 
-  return <h1>{score}</h1>;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> {score}</h1>
+        {/* update the score state */}
+        <button onClick={() => setScore(score + 1)}>
+          Increase score
+        </button>
+      </header>
+    </div>
+  );
 }
 
 export default App;
