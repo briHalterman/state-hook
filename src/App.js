5bb51f6a83e374385f2ab384ff5048ed8a2a62ba
Brittany Halterman <halterma@gmail.com>
2025-03-09 12:10:07 -0400

    Declare multiple states

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import './App.css';
 
 function App() {
   const [score, setScore] = useState(0); // [0, ƒ]
+  const [message, setMessage] = useState('Welcome');
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1> {score}</h1>
+        <h1>{message}</h1>
+        <h2> {score}</h2>
+
         {/* update the score state */}
         <button onClick={() => setScore(score + 1)}>
           Increase score
